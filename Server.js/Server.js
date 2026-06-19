97171ac976d3ccdff62e79f902f64b64f0bbd45f
Sai Spoorthy Eturu <saispoorthyeturu6@gmail.com>
2026-06-19 13:53:22 +0530

    Meaningful message

--- Server.js/Server.js
+++ Server.js/Server.js
@@ -1,10 +1,15 @@
-const express=require("express");
-const app=express();
-const port=process.env.PORT||6700;
+const express = require("express");
 
-//To tell app,where static files-html/css/js are stored
+const app = express();
+
 app.use(express.static("frontend"));
 
-app.listen(port,function(){
-    console.log("App running on http://localhost:"+port);
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/frontend/index.html");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
